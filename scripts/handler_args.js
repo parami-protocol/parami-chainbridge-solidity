00badb61b2ca302b9b7c8ed91ed1c197e1ecddf7
RightSun <szz@joymoe.com>
2021-12-15 20:51:20 +0800

    add handler verify script

---
+++ scripts/handler_args.js
@@ -0,0 +1,7 @@
+const bridge = '0xDb59E0cA1644D1F93D1b85261c0D414656bA98F9';
+module.exports = [
+    bridge,
+    [],
+    [],
+    []
+];
--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 npx hardhat verify --network rinkeby --constructor-args scripts/args.js 0xDb59E0cA1644D1F93D1b85261c0D414656bA98F9
+npx hardhat verify --network rinkeby --constructor-args scripts/handler_args.js 0xDcBa51799a9d411Af5400e760573aBbf3611d725
